9a4f59229bb6b75e53b098caa329290d654b9482
hanif <hanifmutaz@gmail.com>
2026-06-16 13:26:57 +0700

    update server supabase

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -12,12 +12,11 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: Record<string, unknown> }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {}
+              supabaseResponse.cookies.set(name, value, options));
+          } catch { }
         },
       },
     }
